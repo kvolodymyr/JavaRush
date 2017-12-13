122cbbab31879de25f3597ab17612034d302dc21
Volodymyr Kulibaba <kvladimir2007@gmail.com>
2017-12-13 14:38:54 +0200

    Core #20..

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2024/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task20.task2024;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Знакомство с графами
+*/
+public class Solution implements Serializable {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+        /*Solution solution = new Solution();
+        ObjectOutputStream obj = new ObjectOutputStream(new FileOutputStream("fileName"));
+        obj.defaultWriteObject();
+        obj.close();*/
+    }
+}
